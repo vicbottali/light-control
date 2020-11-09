30d7cd30dd273b0bff2587318bbed4fe675dc83a
victoria bottali <victoriab@sca-corp.com>
2020-11-09 16:59:12 -0500

    initial api and react

--- server/express-server.js
+++ server/express-server.js
@@ -6,8 +6,26 @@ const app = express()
 const port = 8000
 const path = require('path')
 const cors = require("cors")
+const MainControl = require('./main.js');
 
-app.use(cors());
+const controller = new MainControl();
+controller.initialize().then(_ => {
+    app.use(cors());
+
+    app.listen(port, () => {
+        console.log(`Example app listening at http://localhost:${port}`)
+    });
+      
+    app.get("/devices", (req, res) => {
+        res.send(controller.deviceInfo);
+    });
+
+    app.get("/scan", (req, res) => {
+        res.send(controller.discoveredDevices);
+    });
+});
+
+
 
 //app.use(express.static("public"));
 
@@ -16,11 +34,3 @@ app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, "public", "index.html"));
 })
 */
-
-app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
-});
-
-app.get("/", (req, res) => {
-  res.send({ message: "We did it!" });
-});
--- src/App.js
+++ src/App.js
@@ -1,15 +1,65 @@
 
-import React, { Component} from "react";
+import React, { Component } from "react";
 import "./styles.css";
+import Axios from "axios";
+import { hot } from "react-hot-loader";
 
-class App extends Component{
-  render(){
-    return(
-      <div className="App">
-        <h1> Hello, World! </h1>
-      </div>
-    );
-  }
+class App extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            devices: {},
+            availableDevices: {}
+        }
+    }
+
+    // Fires after component is added to DOM tree
+    componentDidMount() {
+        Axios({
+            method: "GET",
+            url: "http://localhost:8000/devices",
+            headers: {
+                "Content-Type": "application/json"
+            }
+        }).then(res => {
+            console.log(res.data);
+            this.setState({ devices: res.data });
+        });
+
+        Axios({
+            method: "GET",
+            url: "http://localhost:8000/scan",
+            headers: {
+                "Content-Type": "application/json"
+            }
+        }).then(res => {
+            console.log(res.data);
+            this.setState({ availableDevices: res.data });
+        });
+    }
+
+    renderDevice(devices) {
+        return Object.keys(devices).map((d) => {
+            return (
+                <div className="device-card" key={d}>
+                    <p>{devices[d].name}</p>
+                </div>
+            )
+        })
+    }
+
+    render() {
+        return (
+            <div>
+                <header>
+                    <h1> Lights </h1>
+                </header>
+                <div class="flex-container">
+                    {this.renderDevice(this.state.devices)}
+                </div>
+            </div>
+        );
+    }
 }
 
-export default App;
+export default hot(module)(App);
